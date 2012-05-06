067c09fcd4914ba62c7d1a9b920c25a45b8f6543
Resah Bee <taratbrown@gmail.com>
2012-05-06 13:18:40 -0500

    added a php file to create php class.. need to change footer of  index.php to white and change text to black.. also the columns need to be faded to flow into the white

---
+++ NotizBlock/functions.php
@@ -0,0 +1,6 @@
+<?php
+class functions {
+    //put your code here
+}
+
+?>
--- NotizBlock/index.php
+++ NotizBlock/index.php
@@ -11,7 +11,14 @@
 
 <body>
         <div id="topContentWrap">
-                <div id="header"> <h1 style="text-align: center">NOTIZ BLOCK</h1> </div>
+                <div id="header"> 
+                    <div><h1 style="text-align: center">NOTIZ BLOCK</h1> 
+                        <a id="loginposition" href="" style="text-align:right"> Login  </a>
+                    </div>
+                <br/>
+                <img src="images/account_line.gif" alt=""/> 
+                </div>
+                
 
             <div id="logoPanel"></div>
             <div id="topContent">
@@ -57,5 +64,6 @@
                     
                 </div>
             </div>
+        </div>
     </body>
 </html>
