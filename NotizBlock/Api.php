f590dcc143dea99077ef77983ac84847d3fa2b9d
Resah Bee <taratbrown@gmail.com>
2012-05-07 18:52:00 -0500

    add registered user listbooks and partial add book to api

--- NotizBlock/Api.php
+++ NotizBlock/Api.php
@@ -21,7 +21,7 @@ class Api
     }
     
     public function login($username, $password)
-    {
+    {   
         $sql = "SELECT idnumber, userAccess FROM idpasswords WHERE idnumber='" .  mysql_real_escape_string($username) . "' and idpassword='" .  mysql_real_escape_string($password) . "'";
         
         $result = mysql_query($sql);
@@ -54,14 +54,45 @@ class Api
     
     public function logout()
     {
-        
+        Session.RemoveAll (); //Removes all session variables
     }
     
-    public function registerUser($firstName, $lastName,$password,)
+    public function registerUser($firstName, $lastName,$password,$email,$phone,$personalinfo)
     {
         //add data to the database
+        $sql2= "SELECT userid from basic User where email= '".$email."' ";
+        $alreadyUser = mysql_query($sql2);
+        if ($alreadyUser==null){
+            $sql = "INSERT INTO basicUser VALUES '". mysql_real_escape_string($firstName)."'
+                        , '".mysql_real_escape_string($lastName)."'
+                        , '".mysql_real_escape_string($password)."'
+                        , '".mysql_real_escape_string($email)."'
+                        , '". mysql_real_escape_string($phone)."'
+                        , '". mysql_real_escape_string($personalinfo)."'";
+                $result = mysql_query($sql);
+                if(!$result)
+                {
+                    $err = mysql_error();
+                    print $err;
+                    exit();
+                }
+
+                while($row = mysql_fetch_array($result))
+                {
+                    //echo $row['username'];
+                    if ($row['email'] == $email)
+                    {
+                        $_SESSION['sessionId'] = mysql_query($sql2);//SETTING SESSION ID TO USERID
+                    }
+                }
+                
+        }
+        else{
+          //give an error saying an account is already associated with this email  
+        }
         
-        
+            
+            
         //if successfully added, return the id
         
         //if not successfully added return null
@@ -69,13 +100,21 @@ class Api
     
     public function listBooks($filter = array())
     {
-        // check for the relevant keys and add them to the sql query to return 
-        // the books
+        $userid = $_SESSION['sessionId'];
+        $sql2 = "SELECT itemid from book User where bUserid= '".$userid."' ";
+        $userBooks = mysql_query($sql2);
+        if(userBooks==null){
+            //user has no books
+        }
+        else{
+            return userBooks;
+        }
+            
     }
     
-    public function addBook($title, $isbn)
+    public function addBook($title, $author,$publisher,$saletype,$published_date,$edition,$subjectarea,$condition,$askingprice, $description, $image)
     {
-        
+     
     }
     
     public function getBookDetails($bookId)
