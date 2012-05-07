ac29c9e52cea63ba5b1853f8b6ef6ca8b97566c5
Resah Bee <taratbrown@gmail.com>
2012-05-07 14:25:37 -0500

    index and css

---
+++ NotizBlock/index1.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    
+    <head>
+        <?php include 'htmlhead.php'; ?>
+    </head>
+    <!-- ToDo:  need to change the names of the div ids to correspond to the actual panels. -->
+    <!-- ToDo:  need to tidy the css -->
+    
+
+<body>
+        <div id="topContentWrap">
+            
+            <?php include 'header.php'; ?>
+            
+            <div id="logoPanel"></div>
+            <div id="topContent">
+                <div id="livingSpacesPanel">
+                    <div id="home"><a href="#">Housing</a></div>
+                    <h2>Living Spaces</h2>
+                    <h3>Off- campus housing in and around Mona!</h3>
+                    <div class="read"><a href="#">see more</a></div>
+                </div>
+                <div id="bookPanel">
+                    <div id="about"><a href="#">Books</a></div>
+                    <h2>Books</h2>
+                    <h3>New & used text books for Computer Science and Physics students</h3>
+                    <div class="read"><a href="#">see more</a></div>
+                </div>
+                <div id="rightPanel">
+                    <div id="menu">
+                    </div>
+                    <div id="iconLinks">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="middleWrap">
+            <div id="middlePanel">
+            </div>
+            
+            <?php include 'footer.php'; ?>
+            
+        </div>
+    </body>
+</html>
