6e6b0d5e6e37a3b1e7b710d7263312df8fa2829d
Jheanell Mcpherson <jhinnymac@gmail.com>
2012-05-08 12:41:34 -0400

    header

--- NotizBlock/header.php
+++ NotizBlock/header.php
@@ -1,10 +1,9 @@
 <div id="header"> 
     <div>
-        <h1 style="text-align: center">NOTIZ BLOCK</h1> 
         <a id="loginposition" href="" style="text-align:right">Login</a>
     </div>
     
     <br />
     
-    <img src="images/account_line.gif" alt="" /> 
+    <img src="images/header1.jpg" alt="" width="1000" /> 
 </div>
