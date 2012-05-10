a539403af43d5136c313a6662a8f4581f5f59125
Resah Bee <taratbrown@gmail.com>
2012-05-09 22:09:45 -0500

    fix index.php

--- NotizBlock/header.php
+++ NotizBlock/header.php
@@ -2,18 +2,12 @@
     <div style=" border-bottom-style:groove; border-bottom-width:thick ">
         <a id="loginposition" href="" style="text-align:right">Login</a>
         
-        <img  src="images/header1.JPG" alt=""  width="1000" height="150" style="filter:alpha(style=3, opacity=100)"/>
+        <img  src="images/header1.JPG" alt=""  width="1340" height="150" style="filter:alpha(style=3, opacity=100)"/>
         
         <br />
         <img src="images/account_line.gif" alt="" />
      </div>
-    
-<<<<<<< HEAD
-    <br />
-    
-    <img src="images/header1.jpg" alt="" width="1340" /> 
 
-=======
-    
->>>>>>> 1733ab59d9de2362d0a8adcfcb932aa7bee9f9af
+    <br />
+   
 </div>
