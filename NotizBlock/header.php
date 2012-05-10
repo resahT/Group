dbf983b4ca4cf12e39fd2cd811a72af50600426c
Resah Bee <taratbrown@gmail.com>
2012-05-10 10:32:54 -0500

    fix footer and  test login and editting addbook in API

--- NotizBlock/header.php
+++ NotizBlock/header.php
@@ -1,6 +1,22 @@
+<?php session_start() ?>
+
 <div id="header"> 
     <div style=" border-bottom-style:groove; border-bottom-width:thick; left: 0px;z-index: 999;width: 100%;height: 100px;text-align: center;"">
-        <a id="loginposition" href="" style="text-align:right">Login</a>
+        <?php
+            if (isset($_SESSION['user_info'])){
+                $userinfo = $_SESSION['user_info'];
+        ?>        
+                <a id="loginposition" href=" " style="text-align:right"> <?php echo $userinfo['username'] ?></a>
+        <?php      
+            }           
+            else{
+        ?>
+                <a id="loginposition" href="login.php" style="text-align:right">Login</a>
+         <?php
+            }
+               
+        ?>
+         
         <img  src="images/header1.JPG" alt="" style="margin: 0 auto; border: none;width: 100%;height: 100px;" />      
         <br />
         <img src="images/account_line.gif" alt="" />
