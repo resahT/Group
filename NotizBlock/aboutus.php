a4379d2e45f19ff338e203047390d7ea20a8710b
Jheanell Mcpherson <jhinnymac@gmail.com>
2012-05-14 22:01:05 -0400

    sigh

--- NotizBlock/aboutus.php
+++ NotizBlock/aboutus.php
@@ -1,5 +1,14 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+<!-- include jquery library from google code -->
+<script type="text/javascript" src="http://jqueryjs.googlecode.com/files/jquery-1.3.2.js"></script>
+<!-- include jQuery UI from google code -->
+<script type="type/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js"></script>
+<script rc="/scripts/custom.js">
+$.expander.defaults.slicePoint = 120;
+			$(document).ready(function(){
+				$('div.welcomePanel p').expander();
+			});</script>
 	
 	 <head>
         <?php include 'htmlhead.php'; ?>
@@ -13,25 +22,32 @@
 			<div id="welcomePanel">
 					<div id="dimmer">
 						<div id="dimmerText" align="left">
-							<img src="images/welcome.png" alt="Welcome" hspace="10"/><br/>
-							With the objective of: expanding an under utilized market with 
-							the use of internet technology - NotizBlock was born.
-							Spearheaded by students of the UWI MONA Computer Science Department,
-							NotizBlock aims to accomodate the growth of the 'used book' market
-							by making it easy for staff and students alike to buy and sell the 
-							books they need.
-							NotizBlock also caters to the need of those individuals looking for
-							housing/rooms off campus. Yet again, NotizBlock is aiding a very 
-							popular yet under utilized part of campus life.
-							NEED SOME OTHER NICE STUFF TO WRITE.
-							filling up space filling  up space filling up space filling up space
-							filling up space filling  up space filling up space filling up space
-							filling up space filling  up space filling up space filling up space
-							<img src="images/developers.png" alt="developers" vspace="10" hspace="10"><br/>
-							- Tara Brown<br/> - Jheanell Rainford-McPherson<br/> - Andrew Stoddart<br/> - Vaun-Pierre Wynter
+							<p><img src="images/welcome.png" alt="Welcome" hspace="10"/><br/>
+							NotizBlock is an online advertisement and sale platform that seeks
+							to develop the basic necessities of campus life, leaving the rest
+							of the UWI experience up to you...
+							Our categories include: Books and Off-Campus Housing.</p>
+							<p>If you are interested in buying or selling books in any 
+							condition, or if you have the resources to buy and sell housing
+							in and around the Mona area, NotizBlock is the place to be.
+							If you are interested in taking part in any of these categories
+							just click on the tabs found on the menu. Now! The sale process
+							take place in two different ways. The items can either be up for
+							direct sale or up for bidz.
+							A Direct sale would mean, there is one asking price, no bargaining.
+							However, items that are up for bidz, as it suggests would accomodate
+							users bidding for the item which would go to the highest bidder at 
+							the end of the bidding period.</p>
 						</div>
 					</div>
 			</div>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			
 			<?php include 'footer.php'; ?>
 		</div>
 	</body>
